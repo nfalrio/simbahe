c95101c5902edf9c2767372237a16203d9099991
akunrama21 <naufalllrio200@gmail.com>
2025-05-28 14:36:55 +0700

    first commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/index.html", // Adjust this path to match your project structure
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        quicksand: ["Quicksand", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
